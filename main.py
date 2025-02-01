6fb288b42e93c7dd54844fa783f83e88d68508e1
Sam Quatrone <samquatrone@gmail.com>
2025-02-01 15:53:02 -0700

    feat: automate data collection (range over all values)

--- main.py
+++ main.py
@@ -6,28 +6,56 @@ from tqdm import tqdm
 # Import from other files
 from data_processing import load_data_keys
 
+def generate_data(a_range,b_range,p_range,k_range, data_filename):
+    '''
+    Generates data with ranges for each parameter given as a tuple such as a_range = (a_initial, a_final).
+
+    Note that p values represent indices of prime numbers and should always begin with index of 1.
+    Similarly, k should always be >= 1
+    '''
+    a_1, a_2 = a_range
+    b_1, b_2 = b_range
+    p_1, p_2 = p_range
+    k_1, k_2 = k_range
+
+    # Load keys of existing data to avoid redundant computations
+    existing_data_keys = load_data_keys(data_filename)
+
+    with open(data_filename, 'a', newline='') as csvfile:  # FIXME: handle case where file doesn't exist
+        datawriter = csv.writer(csvfile, delimiter=',')
+
+        for a,b,p,k in tqdm(    # wrapper for progress bar
+            product(
+                range(a_1, a_2+1),
+                range(b_1, b_2+1),
+                islice(Primes(), p_1, p_2+1),   # FIXME: Use something with less overhead
+                range(k_1, k_2+1)
+            ), 
+            total=(a_2-a_1+1)*(b_2-b_1+1)*(p_2-p_1+1)*(k_2-k_1+1)  # number of iterations
+        ):
+            # print((a,b,p,k), is_singular(a,b,p,k))
+            if (a,b) == (0,0) or (a,b,p,k) in existing_data_keys or is_singular(a,b,p,k):
+                continue
+            
+            try:
+                H = EllipticCurve(GF(p,k), [a,b])
+                data = (a,b,p,k, H.abelian_group().short_name())
+                datawriter.writerow(data)
+            except Exception as e:
+                pass
+                # print(f'{(a,b,p,k)}:    {e}')
 
 
-a = 1
-b = 0
-max_k = 20
-max_p = 100
 
+def is_singular(a,b,p,k):
+    q = p**k
+    return (4*a**3 + 27*b**2) % q == 0
 
-# Load keys of existing data to avoid redundant computations
-existing_data_keys = load_data_keys('data.csv')
+if __name__ == '__main__':
+    a_range = (-5,5)
+    b_range = (-5,5)
+    p_range = (1,50)
+    k_range = (1,15)
 
-
-with open('data.csv', 'a', newline='') as csvfile:
-    datawriter = csv.writer(csvfile, delimiter=',')
-
-    # Generate elliptic curve data for a,b over the field GF(p^k) 
-    #   for 1<=p<=max_p and 1<=k<=max_k
-    for p, k in tqdm(product(islice(Primes(), 1, max_p+1), range(1,max_k+1)), total=max_k*max_p):
-        if (a,b,p,k) not in existing_data_keys:
-            H = EllipticCurve(GF(p**k), [a,b])
-            data = (a,b,p,k, H.abelian_group().short_name())
-            datawriter.writerow(data)
-            # print('added data:', data)
-        
+    generate_data(a_range, b_range, p_range, k_range, 'data.csv')
 
