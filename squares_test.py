1ad7e944d5618b386f0f5fbb0d8d9c22f60db025
Sam Quatrone <samquatrone@gmail.com>
2025-04-06 12:02:02 -0600

    feat: overhaul squares_test; confirm conjecture within the data

--- squares_test.py
+++ squares_test.py
@@ -9,64 +9,72 @@ import pprint
 Question:
 For a given (a,b), for which prime numbers p is group(a,b,p,1) of the form Z/n + Z/n?
 '''
-
-def produce_square_primes(k=1):
-    df = data_processing.load_data()
-
-    square_primes_dict = {}
-    square_orders_dict = {}
-
-    # TODO: to generalize for any k, change here:
-    for (a,b), ab_df in df[df['k'] == k].groupby(['a','b']):    
-
-        ab_df['group_struc_1'] = ab_df['group_struc_1'].apply(int)
-        square_primes = list(ab_df.loc[(ab_df['group_struc_1'] == ab_df['group_struc_2'])]['p'].values)
-        square_orders = list(ab_df.loc[(ab_df['group_struc_1'] == ab_df['group_struc_2'])]['group_order'].values)
-        
-        square_primes_dict[(a,b)] = square_primes
-        square_orders_dict[(a,b)] = square_orders
+df = data_processing.load_data()
 
 
-    pprint.pp(square_primes_dict)
-# pprint.pp(square_orders_dict)
+# Abstract groups/conditions
 
-'''
-It seems that this has something to do with supersingular curves, or when the size of group is 1 less than 
-n^2 
-'''
-def find_a_scores():
-    df = data_processing.load_data()
+def is_group_square(row):
+    return row['group_struc_1'] == row['group_struc_2']
 
-    # find where group_order = p^k + 1
-    # find where group_order = p^k - 1
 
-    # df['a_score'] = df.apply(lambda row: (row['p']**row['k'] + 1 - int(row['group_order'])) , axis=1)
-    # df['mod_a_score'] = df.apply(lambda row: row['a_score'] % row['p'], axis=1)
-    # df['max_a_score'] = df.apply(lambda row: int(np.floor(2*int(np.sqrt(float(row['p']**row['k']))))), axis=1)
-    # df['min_a_score'] = df.apply(lambda row: int(np.ceil(-2*int(np.sqrt(float(row['p']**row['k']))))), axis=1)
+def is_group_struc_equal(row, group_struc_1, group_struc_2):
+    return row['group_struc_1'] == group_struc_1 and row['group_struc_2'] == group_struc_2
 
-    # min_a_score_rows = df[df.apply(lambda row: np.ceil(-2*int(np.sqrt(float(row['p']**row['k'])))), axis=1) == df['a_score']]
+def is_trivial_group(row):
+    return is_group_struc_equal(row,1,1)
 
-    # supersingular_rows = df[df['a_score'] == 0]
-    # suprasingular_rows = df[
-    #     (df['a_score'] == 2) & 
-    #     (df['k'] == 1) & 
-    #     ((df['a'] != 0) & df['b'] != 0) & 
-    #     (df['group_struc_1'] == df['group_struc_2'])
-    # ]
 
-    counterexamples = df[
-        (df['group_struc_1'] == df['group_struc_2']) &
-        ((df['k'] != 1) | (df['b'] != 0) | (df['a_score'] != 2)) &
-        ((df['k'].apply(lambda k: k % 2) == 1) | (df['mod_a_score'] != 0)) &
-        (df['a'] != 0) 
-        # ((df['a_score'] != df['min_a_score']) & (df['a_score'] != df['max_a_score']))
+# (a, 0 mod p, p, 1) and p=n^2+1
+def is_square_case_1(row):
+    if (row['b'] % row['p']) != 0 or row['k'] != 1:  
+        return False
+    return row['p'] == (row['group_struc_1']**2 + 1)  
+  
+# (0 mod p, b, p, 1) and p=n^2+n+1, n^2-n+1
+def is_square_case_2(row):
+    if (row['a'] % row['p']) != 0 or row['k'] != 1:  
+        return False
+    n = row['group_struc_1']
+    val_1 = n**2 + n + 1
+    val_2 = n**2 - n + 1
+    return row['p'] == val_1 or row['p'] == val_2
+
+# Supersingular in even column
+def is_square_case_3(row):
+    is_even = (row['k'] % 2) == 0
+    is_supersingular = (row['group_order'] % row['p']) == 1
+    return is_even and is_supersingular
+
+# 
+def is_square_case_4(row):
+    return (row['k'] == 3 and 
+            (row['a'] % 7) == 0 and 
+            is_group_struc_equal(row,18,18))
+
+def is_square_case_5(row):
+    return (row['k'] == 1 and 
+            is_group_struc_equal(row,2,2) and 
+            (row['a'] % row['p'] == 0 or row['b'] % row['p'] == 0))
+
+
+
+# Conjecture: Square => one of the cases holds
+square_df = df[df.apply(is_group_square, axis=1)]
+
+# Is square, but none of the cases hold.
+counter_examples = square_df[
+        square_df.apply(lambda row: not any(
+            [
+                is_square_case_1(row), 
+                is_square_case_2(row),
+                is_square_case_3(row),
+                is_square_case_4(row),
+                is_square_case_5(row),
+                is_trivial_group(row)
+            ]
+        ), axis=1)
     ]
 
-    # max_a_score_rows = df[df['a_score'] == df.apply(lambda row: np.floor(2*int(np.sqrt(float(row['p']**row['k'])))), axis=1 )]
-    # case_1_failure_rows = df[(df.apply(lambda row: gcd(row['p'], row['a_score']), axis=1) != 1) & (df['a_score'] != 0)]
-    print(counterexamples.to_string())
-
-find_a_scores()
-
+print(counter_examples)
 
